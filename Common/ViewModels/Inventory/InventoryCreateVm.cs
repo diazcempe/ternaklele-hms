8b32a3b6d428ae0e0990c18ec7ce3432aad60451
Nastio Diaz <diaz.cmp@gmail.com>
2018-08-03 14:47:57 +0800

    Implemented Inventory Create and Details View Models

---
+++ Common/ViewModels/Inventory/InventoryCreateVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation.Attributes;
+
+namespace Common.ViewModels.Inventory
+{
+    [Validator(typeof(InventoryCreateVmValidation))]
+    public class InventoryCreateVm
+    {
+        public long InventoryId { get; set; }
+        public string CrossReference { get; set; }
+        public string Description { get; set; }
+        public string DistributionUnit { get; set; }
+        public string InventoryType { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public double Quantity { get; set; }
+        public string Rank { get; set; }
+        public double ReorderPoint { get; set; }
+    }
+}
