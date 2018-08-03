ddb31fb053a76fc5a1b31953da8ef23e6a008030
Nastio Diaz <diaz.cmp@gmail.com>
2018-08-03 14:50:48 +0800

    Implemented Fluent Validation for InventoryCreate

---
+++ Common/ViewModels/Inventory/InventoryVmValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+
+namespace Common.ViewModels.Inventory
+{
+    public class InventoryCreateVmValidation : AbstractValidator<InventoryCreateVm>
+    {
+        public InventoryCreateVmValidation()
+        {
+            RuleFor(vm => vm.Name).NotEmpty();
+        }
+    }
+}
