6f527f8993ac2d273c7a36aacc8b7e4c6eb7f267
Nastio Diaz <diaz.cmp@gmail.com>
2018-08-13 19:54:03 +0800

    ORM: Added Inventories to Database

---
+++ Api/Migrations/20180811105517_AddedInventories.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class AddedInventories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    InventoryId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CrossReference = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    DistributionUnit = table.Column<int>(nullable: false),
+                    InventoryType = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Quantity = table.Column<double>(nullable: false),
+                    Rank = table.Column<int>(nullable: false),
+                    ReorderPoint = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.InventoryId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Inventories",
+                columns: new[] { "InventoryId", "CrossReference", "Description", "DistributionUnit", "InventoryType", "Name", "Price", "Quantity", "Rank", "ReorderPoint" },
+                values: new object[] { 1L, "Test", "fwefwe", 0, 0, "cdscsdc", 5.25m, 2.0, 1, 3.0 });
+
+            migrationBuilder.InsertData(
+                table: "Inventories",
+                columns: new[] { "InventoryId", "CrossReference", "Description", "DistributionUnit", "InventoryType", "Name", "Price", "Quantity", "Rank", "ReorderPoint" },
+                values: new object[] { 2L, "vdfvd", "bdbd", 1, 1, "vdfvf", 7.34m, 6.0, 2, 11.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories");
+        }
+    }
+}
--- Api/Models/TernakLeleHmsContext.cs
+++ Api/Models/TernakLeleHmsContext.cs
@@ -1,15 +1,87 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using Common.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Models
 {
     public class TernakLeleHmsContext : DbContext
     {
         public TernakLeleHmsContext(DbContextOptions<TernakLeleHmsContext> options)
-            : base(options) { }
-        
-        public TernakLeleHmsContext() : base() { }
+            : base(options)
+        {
+        }
+
+        //public TernakLeleHmsContext() : base() { }
 
         public DbSet<Inventory> Inventories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigureInventories(modelBuilder);
+        }
+
+        private static void ConfigureInventories(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inventory>().HasKey(t => t.InventoryId);
+            modelBuilder.Entity<Inventory>().HasData(
+                new Inventory
+                {
+                    InventoryId = 1,
+                    CrossReference = "Test",
+                    DistributionUnit = DistributionUnit.Ampoule,
+                    Description = "fwefwe",
+                    InventoryType = InventoryType.Medication,
+                    Name = "cdscsdc",
+                    Price = new decimal(5.25),
+                    Quantity = 2,
+                    Rank = Rank.B,
+                    ReorderPoint = 3
+                },
+                new Inventory
+                {
+                    InventoryId = 2,
+                    CrossReference = "vdfvd",
+                    DistributionUnit = DistributionUnit.Bag,
+                    Description = "bdbd",
+                    InventoryType = InventoryType.Supply,
+                    Name = "vdfvf",
+                    Price = new decimal(7.34),
+                    Quantity = 6,
+                    Rank = Rank.C,
+                    ReorderPoint = 11
+                }
+            );
+        }
+
+        private static IEnumerable<Inventory> InventorySeeds()
+        {
+            return new List<Inventory>
+            {
+                new Inventory()
+                {
+                    CrossReference = "Test",
+                    DistributionUnit = DistributionUnit.Ampoule,
+                    Description = "fwefwe",
+                    InventoryType = InventoryType.Medication,
+                    Name = "cdscsdc",
+                    Price = new decimal(5.25),
+                    Quantity = 2,
+                    Rank = Rank.B,
+                    ReorderPoint = 3
+                },
+                new Inventory()
+                {
+                    CrossReference = "vdfvd",
+                    DistributionUnit = DistributionUnit.Bag,
+                    Description = "bdbd",
+                    InventoryType = InventoryType.Supply,
+                    Name = "vdfvf",
+                    Price = new decimal(7.34),
+                    Quantity = 6,
+                    Rank = Rank.C,
+                    ReorderPoint = 11
+                }
+            };
+        }
     }
 }
